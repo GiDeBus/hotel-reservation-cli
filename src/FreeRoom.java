7fe70c32faec212074a8a8a9e01d73f62cc9c677
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-06 17:25:19 -0400

    add FreeRoom class

--- src/FreeRoom.java
+++ src/FreeRoom.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class FreeRoom {
+import models.RoomType;
+
+public class FreeRoom extends Room {
+    public FreeRoom(String roomNumber, Double price, RoomType enumeration) {
+        super();
+        this.roomNumber = roomNumber;
+        this.price = 0.0;
+        this.enumeration = enumeration;
+    }
+
+    @Override
+    public String toString() {
+        return super().toString() + " Room Number: " + roomNumber + " Room Price: " + "Free" + " Room Type: " + enumeration;
+    }
 }
