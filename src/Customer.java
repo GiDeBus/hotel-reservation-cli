6e96814cd73bd2d0927daa9e0a01759f163c1fa3
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-06 17:57:08 -0400

    add Customer class with email validation

--- src/Customer.java
+++ src/Customer.java
@@ -1,10 +1,16 @@
+import java.util.regex.Pattern;
+
 public class Customer {
     private String firstName;
     private String lastName;
     private String email;
+    private final String emailRegex = "^(.+)@(.+).com$";
+    private final Pattern pattern = Pattern.compile(emailRegex);
 
     public Customer(String firstName, String lastName, String email) {
-
+        if(!pattern.matcher(email).matches()) {
+            throw new IllegalArgumentException("Invalid email format.");
+        }
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
