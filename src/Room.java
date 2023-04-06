35cbca53ba1d09ecc21ac45a269972ed55ac286d
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-06 17:25:03 -0400

    add Room class

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Room {
+import models.IRoom;
+import models.RoomType;
+
+import java.util.Enumeration;
+
+public class Room implements IRoom {
+    private String roomNumber;
+    private Double price;
+    private RoomType enumeration;
+
+    public Room(String roomNumber, Double price, RoomType enumeration) {
+        super();
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.enumeration = enumeration;
+    }
+    @Override
+    public String toString() {
+        return super().toString() + " Room Number: " + roomNumber + " Room Price: " + price + " Room Type: " + enumeration;
+    }
 }
