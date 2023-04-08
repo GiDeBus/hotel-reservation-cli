34f38592943ec00b164e1fe2a51bb4275dbce613
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-08 10:49:49 -0400

    add CustomerService class

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,2 +1,28 @@
-package service;public class CustomerService {
+package service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import classes.Customer;
+
+public class CustomerService {
+
+    private static CustomerService customerService = new CustomerService();
+    private static final Map<String, Customer> customers = new HashMap<>();
+
+    // Instead of getInstance() method
+    public static CustomerService getCustomerService() {
+        return customerService;
+    }
+    public void addCustomer(String email, String firstName, String lastName) {
+        Customer customer = new Customer(email, firstName, lastName);
+        customers.put(email, customer);
+    }
+    public Customer getCustomer(String customerEmail) {
+        return customers.get(customerEmail);
+    }
+    public Collection<Customer> getAllCustomers() {
+        return customers.values();
+    }
 }
