12a5f5d44da5e5d02725ffbca1a68e428733dbb9
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-11 17:55:13 -0400

    implement getCustomer

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -1,8 +1,14 @@
 package api;
 
+import services.CustomerService;
+import classes.Customer;
 public class AdminResource {
     private static AdminResource adminResource = new AdminResource();
     public static AdminResource getAdminResource() {
         return adminResource;
+    }
+
+    public Customer getCustomer(String email) {
+        return CustomerService.getCustomerService().getCustomer(email);
     }
 }
