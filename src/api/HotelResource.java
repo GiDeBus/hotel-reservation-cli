2ce38c135b91b1135450a1537938792082b1cff0
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-11 17:24:50 -0400

    add getCustomer

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,8 +1,14 @@
 package api;
+import services.CustomerService;
+import classes.*;
 
 public class HotelResource {
     private static HotelResource hotelResource = new HotelResource();
     public static HotelResource getHotelResource() {
         return hotelResource;
+    }
+
+    public Customer getCustomer(String email) {
+        CustomerService.getCustomerService().getCustomer(email);
     }
 }
