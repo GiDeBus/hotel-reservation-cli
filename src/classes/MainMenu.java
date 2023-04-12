cc2834e196f6a7eb76cbea7f9ec289eab3dd7630
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-12 17:09:05 -0400

    implement MainMenu class

--- src/classes/MainMenu.java
+++ src/classes/MainMenu.java
@@ -1,6 +1,32 @@
 package classes;
 
 import java.util.Scanner;
+import services.ReservationService
 public class MainMenu {
 
+    public static void main(String[] args) {
+        boolean keepRunning = true;
+
+        try(Scanner scanner = new Scanner(System.in)) {
+            while(keepRunning) {
+                try {
+                    System.out.println("Main Menu");
+                    System.out.println("------------------------------------------------");
+                    System.out.println("1. Find and reserve a room");
+                    System.out.println("2. See my reservations");
+                    System.out.println("3. Create an account");
+                    System.out.println("4. Admin");
+                    System.out.println("5. Exit");
+                    System.out.println("------------------------------------------------");
+                    System.out.println("Please select a number from the menu option");
+
+                    int selection = Integer.parseInt(scanner.nextLine());
+                } catch(Exception exception) {
+                    System.out.println("\nError - Invalid Input\n");
+                } finally {
+                    scanner.close();
+                }
+            }
+        }
+    }
 }
